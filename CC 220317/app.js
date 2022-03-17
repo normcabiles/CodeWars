23f605934212990b3cf8b1b7ee3b941e01947724
Norm Cabiles <cabilesn@gmail.com>
2022-03-17 07:03:34 -0700

    Coding challenge and solution for 3-17-22.

---
+++ CC 220317/app.js
@@ -0,0 +1,14 @@
+// class Kata {
+//     static getVolumeOfCuboid(length, width, height) {
+//       // your code here
+//     }
+//   }
+
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      // your code here
+      const getVolumeOfCuboid = length * width * height
+      return getVolumeOfCuboid
+    }
+  }
